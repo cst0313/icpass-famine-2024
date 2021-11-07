73cd1fe02da59e2cd828c5db6deac67c4fabee7f
N00bAdrian <adrianwong227@gmail.com>
2021-11-07 23:14:22 +0000

    Connected to firebase firestore

--- famine-lifemon/src/components/Stats.js
+++ famine-lifemon/src/components/Stats.js
@@ -3,68 +3,65 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
+import { doc, DocumentSnapshot } from 'firebase/firestore';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
 
-class Stats extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			name: "Adrian Wong",
-			health: 10,
-			happiness: 10,
-			money: 1500,
-			health_symbol: "❤️",
-			happy_symbol: "😄"
-		}
+import { db } from '../database/firebase';
+
+export default function Stats() {
+	const [snapshot, loading, error] = useDocumentData(
+		doc(db, "users", "euLcu7FQBzyTjYprBpzT")
+	);
+	if (error) {
+		console.log(JSON.stringify(error));
 	}
-
-	render () {
-		return (
-			<Card variant="outlined">
-				<Grid container spacing={2} fontSize={20}>
-					<Grid item xs={3}>
-						<Typography variant="body1">
-							Name: 
-						</Typography>
-					</Grid>
-					<Grid item xs={8}>
-						<Typography variant="body1">
-							{this.state.name}
-						</Typography>
-					</Grid>
-					<Grid item xs={3}>
-						<Typography variant="body1">
-							Health: 
-						</Typography>
-					</Grid>
-					<Grid item xs={8}>
-						<Typography variant="body1">
-							{this.state.health_symbol.repeat(this.state.health)}
-						</Typography>
-					</Grid>
-					<Grid item xs={3}>
-						<Typography variant="body1">
-							Happniess: 
-						</Typography>
-					</Grid>
-					<Grid item xs={8}>
-						<Typography variant="body1">
-							{this.state.happy_symbol.repeat(this.state.happiness)}
-						</Typography>
-					</Grid>
-					<Grid item xs={3}>
-						<Typography variant="body1">
-							Money:
-						</Typography>
-					</Grid>
-					<Grid item xs={8}>
-						<Typography variant="body1">
-							{"$" + this.state.money}
-						</Typography>
-					</Grid>
+	console.log(snapshot);
+	const happy_symbol = "😄";
+	const health_symbol = "❤️";
+	return (
+		<Card variant="outlined">
+			<Grid container spacing={2} fontSize={20}>
+				<Grid item xs={3}>
+					<Typography variant="body1">
+						Name: 
+					</Typography>
 				</Grid>
-			</Card>
-		);
-	}
+				<Grid item xs={8}>
+					<Typography variant="body1">
+						Adrian Wong
+					</Typography>
+				</Grid>
+				<Grid item xs={3}>
+					<Typography variant="body1">
+						Health: 
+					</Typography>
+				</Grid>
+				<Grid item xs={8}>
+					<Typography variant="body1">
+						{health_symbol.repeat(10)}
+					</Typography>
+				</Grid>
+				<Grid item xs={3}>
+					<Typography variant="body1">
+						Happniess: 
+					</Typography>
+				</Grid>
+				<Grid item xs={8}>
+					<Typography variant="body1">
+						{happy_symbol.repeat(10)}
+					</Typography>
+				</Grid>
+				<Grid item xs={3}>
+					<Typography variant="body1">
+						Money:
+					</Typography>
+				</Grid>
+				<Grid item xs={8}>
+					<Typography variant="body1">
+						{"$" + 1500}
+					</Typography>
+				</Grid>
+			</Grid>
+		</Card>
+	);
 }
-
-export default Stats;
