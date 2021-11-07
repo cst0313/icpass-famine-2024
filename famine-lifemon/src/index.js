df2c7e5bd6c754fc17fa6fe7ad0f7d91366e8c9c
N00bAdrian <adrianwong227@gmail.com>
2021-11-07 00:58:31 +0000

    Implemented prototype title card

--- famine-lifemon/src/index.js
+++ famine-lifemon/src/index.js
@@ -2,6 +2,33 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+import '@fontsource/roboto/400.css';
+import './index.css';
+
+function Title() {
+	return (
+		<Grid container spacing={2}>
+			<Grid item xs={4}>
+				<img
+					className = "logo" 
+					src = "passlogo.png" 
+					alt = "ICPASS logo"
+				/>
+			</Grid>
+			<Grid item className="title" xs={8}>
+				<Typography
+					variant="h4"
+					fontSize={30}
+				>
+					ICPASS Famine 24 Passport
+				</Typography>
+			</Grid>
+		</Grid>
+	);
+}
 
 function App() {
 	return (
@@ -14,6 +41,9 @@ function App() {
 }
 
 ReactDOM.render(
-	<App />, 
+	<>
+		<Title />	
+		<App /> 
+	</>,
 	document.getElementById('root')
 );
