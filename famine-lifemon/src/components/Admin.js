3144ffefe594e52f7ea8117d576a6fafb96ce2ff
N00bAdrian <adrianwong227@gmail.com>
2021-11-14 18:14:30 +0000

    Implemented sliders for money, happiness, health

--- famine-lifemon/src/components/Admin.js
+++ famine-lifemon/src/components/Admin.js
@@ -1,14 +1,133 @@
 import * as React from 'react';
 
-import { Typography, Card } from '@mui/material';
+import { Typography, Card, Accordion, AccordionSummary, AccordionDetails, Slider, Input, Grid } from '@mui/material';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function Admin() {
+	const [formData, setFormData] = React.useState({
+		happiness : 0,
+		health : 0,
+		money : 0
+	});
+	function handleHBlur () {
+		if (formData.happiness < -5) {
+			setFormData({formData, happiness: -5});
+		} else if (formData.happiness > 5) {
+			setFormData({formData, happiness: 5});
+		}
+	}
 	return (
 		<div>
 			<Card variant="outlined">
-				<Typography variant="h1">
-					Admin Page
-				</Typography>
+				<Accordion>
+					<AccordionSummary expandIcon={<ExpandMoreIcon />} >
+						<Typography>General settings</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Typography>
+							Happiness
+						</Typography>
+						<Grid container spacing={2}>
+							<Grid item xs={10}>
+								<Slider 
+									value={formData.happiness}
+									onChange={
+										(e) => setFormData({...formData, happiness: e.target.value})
+									}
+									step={1}
+									min={-5}
+									max={5}
+								/>
+							</Grid>
+							<Grid item xs={2}>
+								<Input 
+									fullWidth
+									value={formData.happiness}
+									size="small"
+									onChange={
+										(e) => setFormData({...formData, happiness: e.target.value})
+									}
+									onBlur={handleHBlur}
+									inputProps={{
+										step: 1,
+										min: -5,
+										max: 5,
+										type: 'number',
+									}}
+								/>
+							</Grid>
+						</Grid>
+
+						<Typography>
+							Health
+						</Typography>
+						<Grid container spacing={2}>
+							<Grid item xs={10}>
+								<Slider 
+									value={formData.health}
+									onChange={
+										(e) => setFormData({...formData, health: e.target.value})
+									}
+									step={1}
+									min={-5}
+									max={5}
+								/>
+							</Grid>
+							<Grid item xs={2}>
+								<Input 
+									fullWidth
+									value={formData.health}
+									size="small"
+									onChange={
+										(e) => setFormData({...formData, health: e.target.value})
+									}
+									onBlur={handleHBlur}
+									inputProps={{
+										step: 1,
+										min: -5,
+										max: 5,
+										type: 'number',
+									}}
+								/>
+							</Grid>
+						</Grid>
+
+						<Typography>
+							Money
+						</Typography>
+						<Grid container spacing={2}>
+							<Grid item xs={10}>
+								<Slider 
+									value={formData.money}
+									onChange={
+										(e) => setFormData({...formData, money: e.target.value})
+									}
+									step={100}
+									min={-1500}
+									max={1500}
+								/>
+							</Grid>
+							<Grid item xs={2}>
+								<Input 
+									fullWidth
+									value={formData.money}
+									size="small"
+									onChange={
+										(e) => setFormData({...formData, money: e.target.value})
+									}
+									onBlur={handleHBlur}
+									inputProps={{
+										step: 100,
+										min: -1500,
+										max: 1500,
+										type: 'number',
+									}}
+								/>
+							</Grid>
+						</Grid>
+					</AccordionDetails>
+				</Accordion>
 			</Card>
 		</div>
 	);
