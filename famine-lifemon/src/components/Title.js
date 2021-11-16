c560dd69c4f254b983b208444b465226788fa27e
Brigidw <brigidwong@gmail.com>
2021-11-16 20:57:52 +0000

    change layout and color

--- famine-lifemon/src/components/Title.js
+++ famine-lifemon/src/components/Title.js
@@ -2,39 +2,53 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { Box } from '@mui/system';
+import { ThemeProvider,createTheme } from '@mui/material/styles';
 
-export default function Title() {
-	return (
-		<Box sx={{background:"#1F3149", p:0,m:-2}}>
-			<Grid container spacing={2} direction="row" >
-				<Grid item className="title" xs={5} p={0} style={{justifyContent:"flex-end", alignItems:"center"}}>
-					<Typography
-						variant="h5"
-						fontSize={22}
-						fontWeight='bold'
-						lineWeight={1}
-					>
-						Famine 24
-					</Typography>
-				</Grid>	
-				<Grid item xs={2} style={{alignItems:"center"}} p={0}>
-					<img
-						className="logo"
-						src="passlogo.png"
-						alt="ICPASS logo"/>
-				</Grid>
-				<Grid item className="title" xs={5} p={0} style={{justifyContent:"flex-start", alignItems:"center"}}>
-					<Typography
-						variant="h5"
-						fontSize={22}
-						fontWeight='bold'
-						lineHeight={1}
-					>
-						Passport
-					</Typography>
-				</Grid>
-			</Grid>
-		</Box>
-		
-	);
-}
+const theme_1 = createTheme({
+	typography: {
+		fontFamily:'Playfair+Display'
+	}
+})
+
+
+	return(
+		<ThemeProvider theme = {theme_1}>
+		export default function Title() {
+					<Box sx={{background:'#', p:0,m:-2}}>
+						<Grid container spacing={2} direction="row" >
+							<Grid item xs={12}>
+							</Grid>
+							<Grid item className="title" xs={5} p={0} style={{justifyContent:"flex-end", alignItems:"center"}}>
+								<Typography
+									variant="h5"
+									fontSize={22}
+									fontWeight='bold'
+									lineWeight={1}
+								>
+									Famine 24
+								</Typography>
+							</Grid>	
+							<Grid item xs={2} align="center">
+								<img
+									className="logo"
+									src="passlogo.png"
+									alt="ICPASS logo"
+									align="middle"/>
+							</Grid>
+							<Grid item className="title" xs={5} p={0} style={{justifyContent:"flex-start", alignItems:"center"}}>
+								<Typography
+									variant="h5"
+									fontSize={22}						
+									fontWeight='bold'
+									lineHeight={1}
+								>
+									Passport
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+		}	
+	
+	</ThemeProvider>
+	)
+
