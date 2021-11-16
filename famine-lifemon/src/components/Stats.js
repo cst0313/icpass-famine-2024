257dd37d3d260ec4e095483255cb551a31bcebae
Brigidw <brigidwong@gmail.com>
2021-11-16 00:25:36 +0000

    changed bk color

--- famine-lifemon/src/components/Stats.js
+++ famine-lifemon/src/components/Stats.js
@@ -20,67 +20,67 @@ export default function Stats(id) {
 	const healthSymbol = "❤️";
 	const educationMap = ["None", "Secondary", "Bachelor's", "Master's"];
 	return (
-		<Card variant="outlined" sx={{
+		<Card variant="outlined" style={{background: 'linear-gradient(45deg, #F8AA6F 30%, #FFC654 90%)'}} sx={{
 			borderColor: loading ? "" : snapshot.covid ? "error.main" : ""
 		}}>
-			<Grid container spacing={2} fontSize={20}>
+			<Grid container spacing={0.5} fontSize={16} style={{justifyContent:"flex-end", alignItems:"center"}}>
 				<Grid item xs={3}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						Name: 
 					</Typography>
 				</Grid>
 				<Grid item xs={8}>
-					<Typography variant="body1">
+					<Typography variant="body2" fontWeight='bold' color="#19273A">
 						{loading ? "Loading..." : snapshot.name}
 					</Typography>
 				</Grid>
 				<Grid item xs={3}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						Health: 
 					</Typography>
 				</Grid>
 				<Grid item xs={8}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						{loading ? "Loading..." : healthSymbol.repeat(snapshot.health)}
 					</Typography>
 				</Grid>
 				<Grid item xs={3}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						Happiness: 
 					</Typography>
 				</Grid>
 				<Grid item xs={8}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						{loading ? "Loading..." : happySymbol.repeat(snapshot.happiness)}
 					</Typography>
 				</Grid>
 				<Grid item xs={3}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						Money:
 					</Typography>
 				</Grid>
 				<Grid item xs={8}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						{loading ? "Loading..." : "$" + snapshot.money}
 					</Typography>
 				</Grid>
 				<Grid item xs={3}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						Education:
 					</Typography>
 				</Grid>
 				<Grid item xs={8}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						{loading ? "Loading..." : educationMap[snapshot.education]}
 					</Typography>
 				</Grid>
 				<Grid item xs={3}>
-					<Typography variant="body1">
+					<Typography variant="body2">
 						Married?
 					</Typography>
 				</Grid>
 				<Grid item xs={8}>
-					<Typography variant="body1">
+					<Typography variant="body2" >
 						{loading ? "Loading..." : snapshot.married ? "Yes" : "No"}
 					</Typography>
 				</Grid>
